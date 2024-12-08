3046261bb07594b938c25712ade9e47cfccac944
AbdulRafay54 <rafayabdul7817@gmail.com>
2024-12-08 13:15:30 +0500

    first commit

--- index.js
+++ index.js
@@ -1,25 +1,57 @@
-const express = require("express");
+// Express module ko import karna
+import express from "express";
+
 const app = express();
 const port = 3000;
 
+// Middleware setup, jisse hum JSON body ko handle kar sakein
 app.use(express.json());
 
-// GET Route
+// Sample users array
+const users = [
+  {
+    id: 1,
+    name: "Abdul Rafay",
+  },
+  {
+    id: 2,
+    name: "HAmmad Haider",
+  },
+];
+//ye get ka route
 app.get("/", (req, res) => {
-    res.send("Hello, World!");
-});
-
-// POST Route
-app.post("/add", (req, res) => {
-    const { name } = req.body; 
-    if (!name) {
-        return res.status(400).send("Name is required");
-    }
-    res.send(`User ${name} added successfully!`);
+  res.send("Hello World!");
 });
 
 
-//isse server start hoga to pata chal jaega
+//is 'get' se sary user aajaengy add krny k bd
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+//new user k liiye
+app.post("/user", (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({
+      message: "Name is required",
+    });
+  }
+
+  // Naya user create karna
+  users.push({
+    id: Date.now(), 
+    name,
+  });
+
+  res.send({
+    message: "User added successfully",
+    data: users,
+  });
+});
+
+// Server ko start karna
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
